11061548bd1c9a8cc6524f27fb5e994892f79aa5
Abhijeet Gulhane <abhijeet.gulhane.317@gmail.com>
2025-12-25 18:51:06 -0500

    feat: verified remote backend and state locking

---
+++ backend.tf
@@ -0,0 +1,19 @@
+#terraform {
+#  backend "s3" {
+#    bucket         = "abhijeet-terraform-state-2025"
+#    key            = "dev/terraform.tfstate"
+#    region         = "us-east-1"
+#    dynamodb_table = "terraform-state-locking"
+#    encrypt        = true
+#  }
+#}
+
+terraform {
+  backend "s3" {
+    bucket       = "abhijeet-terraform-state-2025"
+    key          = "dev/terraform.tfstate"
+    region       = "us-east-1"
+    # Replacing dynamodb_table with the new recommended locking
+    use_lockfile = true 
+  }
+}
---
+++ backend_setup.tf
@@ -0,0 +1,20 @@
+# 1. The S3 Bucket for State Storage
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "abhijeet-terraform-state-2025" # Must be unique
+  
+  #lifecycle {
+  #  prevent_destroy = true # Safety first!
+  #}
+}
+
+# 2. The DynamoDB Table for Locking
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
