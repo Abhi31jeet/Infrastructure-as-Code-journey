2f791b9ef1dd66303089458f86a908e1e2225f6d
Abhijeet Gulhane <abhijeet.gulhane.317@gmail.com>
2026-02-04 20:28:38 -0500

    Week 6 Redo: Clean refactor with working templates and local variables

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -8,3 +8,4 @@ systemctl enable docker
 aws ecr get-login-password --region ${aws_region} | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.${aws_region}.amazonaws.com
 
 docker run -d -p 80:80 ${account_id}.dkr.ecr.${aws_region}.amazonaws.com/sre-journey-repo:v1
+
